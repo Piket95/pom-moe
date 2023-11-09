aded5ca79623a2f177936a0283483f9123c6e27a
Dennis Adam <dennis.adam95@gmail.com>
2023-11-10 00:56:41 +0100

    changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	preview: {
+		host: "0.0.0.0",
+		port: 80,
+		strictPort: true,
+	},
 });
